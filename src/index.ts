7e2cdf234837092b5d803a0b6deac9b6de34813a
Peter Schwenk <paschwenk@gmail.com>
2026-01-14 21:06:12 -0500

    After "Commands" lesson

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
-import { setUser, readConfig } from "./config"
+import { argv } from "node:process";
+import { readConfig } from "./config"
+import { CommandsRegistry, registerCommand, runCommand } from "./command_handler"
+import { handlerLogin } from "./handlerLogin";
 
 function main() {
-  setUser("petshwark");
-  const configJson = JSON.stringify(readConfig());
-  if (configJson) {
-    console.log('Configuration data:')
-    console.log(configJson);
-  } else {
-    console.error('No configuration data read.')
+  if (argv.length < 3) {
+    console.error(`No arguments passed to the command.`);
+    process.exit(1);
+  }
+  const args = argv.slice(2);
+  const commandName = args[0];
+  const commandRegistry: CommandsRegistry = {};
+  registerCommand(commandRegistry, 'login', handlerLogin);
+  console.log('Registered commands.');
+  try {
+    runCommand(commandRegistry, commandName, ...args.slice(1));
+    const configJson = JSON.stringify(readConfig());
+    if (configJson) {
+      console.log('Configuration data:')
+      console.log(configJson);
+    } else {
+      console.error('No configuration data read.')
+    }
+  } catch {
+    console.error(`Command "${commandName}" failed.`);
+    process.exit(1);
   }
 }
 
