8195a47bceb0fa6efd88e3345f33c57867a2a201
Peter Schwenk <paschwenk@gmail.com>
2026-02-09 16:34:55 -0500

    After "Middleware" lesson

--- src/handlers/command_handler.ts
+++ src/handlers/command_handler.ts
@@ -7,8 +7,12 @@ import { handlerUsers } from "./handler_users";
 import { handlerAddFeed } from "./handler_addfeed";
 import { handleFollow } from "./handler_follow";
 import { handleFollowing } from "./handler_following";
+import { User } from "../lib/db/schema";
+import { getUserByName } from "../lib/db/queries/users";
+import { readConfig } from "../config";
 
 export type CommandHandler = (cmdName: string, ...args: string[]) => Promise<void>;
+export type UserCommandHandler = (cmdName: string, user: User, ...args: string[]) => Promise<void>;
 
 export type CommandsRegistry = Record<string, CommandHandler>;
 
@@ -20,14 +24,33 @@ export async function runCommand(registry: CommandsRegistry, cmdName: string, ..
     await registry[cmdName](cmdName, ...args);
 }
 
+function middlewareLoggedIn(handler: UserCommandHandler): CommandHandler {
+    return async (cmdName: string, ...args: string[]) => {
+        const config = readConfig();
+        if (!config) {
+            throw new Error("User not logged in. Please log in first.");
+        }
+        const userName = config.currentUserName;
+        try {
+            const selectedUser = await getUserByName(userName);
+            if (!selectedUser) {
+                throw new Error("User not found. Please log in first.");
+            }
+            await handler(cmdName, selectedUser, ...args);
+        } catch (error) {
+            throw new Error(`Error executing command ${cmdName}: ${error}`);
+        }
+    };
+}
+
 export function registerCommands(registry: CommandsRegistry): void {
     registerCommand(registry, 'login', handlerLogin);
     registerCommand(registry, 'register', handlerRegister);
     registerCommand(registry, 'reset', handlerReset);
     registerCommand(registry, 'users', handlerUsers);
     registerCommand(registry, 'agg', handlerAgg);
-    registerCommand(registry, 'addfeed', handlerAddFeed);
+    registerCommand(registry, 'addfeed', middlewareLoggedIn(handlerAddFeed));
     registerCommand(registry, 'feeds', handlerFeeds);
-    registerCommand(registry, 'follow', handleFollow);
-    registerCommand(registry, 'following', handleFollowing);
+    registerCommand(registry, 'follow', middlewareLoggedIn(handleFollow));
+    registerCommand(registry, 'following', middlewareLoggedIn(handleFollowing));
 }
